fbff1f341bfb720eeb84222705613aed43bbfe09
Matt Greenfield <gmattgreenfield@gmail.com>
2018-11-24 19:14:58 +0000

    Vue tempalte setup

---
+++ src/components/Heading/Heading.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="heading">{{ text }}</h1>
+</template>
+
+<script>
+import "./Heading.scss";
+
+export default {
+  name: "Heading",
+  props: {
+    text: String
+  }
+};
+</script>
